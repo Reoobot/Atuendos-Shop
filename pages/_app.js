2f3c211091f4ea0c96426618c0b29cca30364bdd
Freddy Vegas <vegasfalexis@gmail.com>
2023-08-06 15:55:05 +0200

    scaloffing

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import { CartContextProvider } from "@/components/CartContext";
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+
+  body{
+    background-color: #eee;
+    padding:0;
+    margin:0;
+    font-family: 'Poppins', sans-serif;
+  }
+`;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    
+      <GlobalStyles />
+      <CartContextProvider>
+        <Component {...pageProps} />
+      </CartContextProvider>
+    </>
+  );
 }
+
---
+++ pages/products.js
@@ -0,0 +1,31 @@
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import ProductsGrid from "@/components/ProductsGrid";
+import Title from "@/components/Title";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+
+
+
+
+export default function ProductsPage({products}) {
+   
+    return (
+        <>
+        <Header/>
+        <Center>
+            <Title>All products</Title>
+            <ProductsGrid products={products}/>
+        </Center>
+        </>
+    );
+}
+export async function getServerSideProps() {
+    await mongooseConnect();
+    const products =  await Product.find({}, null, {sort:{'_id':-1}});
+    return {
+        props: {
+            products:JSON.parse(JSON.stringify(products)),
+        }
+    }
+}
